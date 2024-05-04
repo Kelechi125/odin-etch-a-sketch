9e25c002ea692d7f855b85be81d33960c89c6179
Kelechi125 <kelechi125@yahoo.com>
2024-05-04 13:35:38 -0500

    Add attribute of flex to rowCell to expand the grid to fill the container

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const createGrid = () => {
     for (let i = 0; i < 16; i++) {
         const rowCell = document.createElement("div");
         rowCell.classList.add("rowDiv");
-        rowCell.style.height = "50px";
-        rowCell.style.width = "50px";
-        rowCell.setAttribute("style", "border: 1px solid black; display: flex;");
+        //rowCell.style.height = "50px";
+        //rowCell.style.width = "50px";
+        rowCell.setAttribute("style", "border: 1px solid black; display: flex; gap: 1px; align-content: stretch; flex: 1");
 
-        for (let j = 0; j < 15; j++) {
+        for (let j = 0; j < 16; j++) {
             const columnCell = document.createElement("div");
             columnCell.classList.add("columnDiv");
+            columnCell.setAttribute("style", "border: 1px solid black; flex: 1;");
             rowCell.appendChild(columnCell);
             }
 
