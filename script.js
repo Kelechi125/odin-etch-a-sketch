d04b495c6c4588c1173463b49999e1eeedfdc69d
Kelechi125 <kelechi125@yahoo.com>
2024-05-04 23:33:34 -0500

    Rename 'rowCell' to just 'row'

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const gridContainer = document.querySelector("#grid-container");
 
 const createGrid = () => {
     for (let i = 0; i < 16; i++) {
-        const rowCell = document.createElement("div");
-        rowCell.classList.add("rowDiv");
+        const row = document.createElement("div");
+        row.classList.add("rowDiv");
         //rowCell.style.height = "50px";
         //rowCell.style.width = "50px";
         rowCell.setAttribute("style", "border: 0.25px solid black; display: flex; gap: 1px; align-content: stretch; flex: 1;");
@@ -12,10 +12,10 @@ const createGrid = () => {
             const columnCell = document.createElement("div");
             columnCell.classList.add("columnDiv");
             columnCell.setAttribute("style", "border: 0.25px solid black; flex: 1;");
-            rowCell.appendChild(columnCell);
+            row.appendChild(columnCell);
             }
 
-            gridContainer.appendChild(rowCell);
+            gridContainer.appendChild(row);
         }
     }
 
